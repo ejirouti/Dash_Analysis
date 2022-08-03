ca5d5c57593b7569c9e6244bca456acdc6620328
ejirouti <69774032+ejirouti@users.noreply.github.com>
2022-08-03 11:01:50 -0600

    Update 5_Peer_Graded_Assignment_Questions.py

--- 5_Peer_Graded_Assignment_Questions.py
+++ 5_Peer_Graded_Assignment_Questions.py
@@ -73,7 +73,7 @@ def compute_data_choice_2(df):
 # Application layout
 app.layout = html.Div(children=[html.H1('US Domestic Airline Flight Performance ', 
                                 style={'textAlign': 'center', 'color': '#503D36',
-                                'font-size': 24})],
+                      'font-size': 24}), 
                                 # TASK1: Add title to the dashboard
                                 # Enter your code below. Make sure you have correct formatting.
                                  
@@ -98,7 +98,7 @@ app.layout = html.Div(children=[html.H1('US Domestic Airline Flight Performance 
                   placeholder='Select a report type',
                   style={'width':'80%','padding':'3px','font-size':'20px','text-align-last':'center'})
                                     # Place them next to each other using the division style
-                                    ],
+                                    ], style={'display':'flex'}), 
                                     
                                    # Add next division 
                                    html.Div([
@@ -114,8 +114,8 @@ app.layout = html.Div(children=[html.H1('US Domestic Airline Flight Performance 
                                                      placeholder="Select a year",
                                                      style={'width':'80%', 'padding':'3px', 'font-size': '20px', 'text-align-last' : 'center'}),
                                             # Place them next to each other using the division style
-                                            ], style={'display': 'flex'}),  
-                                        
+                                            ], style={'display': 'flex'}), 
+                                  
                                 
                                 # Add Computed graphs
                                 # REVIEW3: Observe how we add an empty division and providing an id that will be updated during callback
@@ -131,17 +131,17 @@ app.layout = html.Div(children=[html.H1('US Domestic Airline Flight Performance 
                                 html.Div([
                                         html.Div([ ], id='plot4'),
                                         html.Div([ ], id='plot5')
-                                ])
-
+                                ], style={'display': 'flex'}), ])
+                                
 # Callback function definition
 # TASK4: Add 5 ouput components
 # Enter your code below. Make sure you have correct formatting.
-[Output(component_id='plot1', component_property='children'),
+@app.callback( [Output(component_id='plot1', component_property='children'), 
  Output(component_id='plot2', component_property='children'),
  Output(component_id='plot3', component_property='children'),
  Output(component_id='plot4', component_property='children'),
- Output(component_id='plot5', component_property='children')]
-@app.callback(
+ Output(component_id='plot5', component_property='children')] 
+
                [Input(component_id='input-type', component_property='value'),
                 Input(component_id='input-year', component_property='value')],
                # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
@@ -164,7 +164,7 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             
             # TASK5: Average flight time by reporting airline
             # Enter your code below. Make sure you have correct formatting.
-            line_fig = px.line(line_data, x='Month', y='Airtime', color='Reporting_Airline', title='Average monthly flight time (minutes) by airine')
+            line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline', title='Average monthly flight time (minutes) by airline')
             
             # Percentage of diverted airport landings per reporting airline
             pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='% of flights by reporting airline')
